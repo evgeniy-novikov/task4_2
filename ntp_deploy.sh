cbe6633990f5d3efbc67741247bbbbbf184d7e75
root <root@mirantis.test>
2018-03-31 17:30:49 +0300

    work

--- ntp_deploy.sh
+++ ntp_deploy.sh
@@ -4,15 +4,18 @@ apt-get -y install ntp
 
 mkdir /opt/controlled_files/
 
-#echo -e "server ua.pool.ntp.org iburst prefer" ; cat /etc/ntp.conf | grep -v "server "
-sed -i '/server [[:digit:]]\+/d' /etc/ntp.conf
-echo "server ua.pool.ntp.org iburst" >> /etc/ntp.conf
+#echo -e "pool ua.pool.ntp.org iburst prefer" ; cat /etc/ntp.conf | grep -v "server "
+
+sed -i '/pool \+/d' /etc/ntp.conf
+echo "pool ua.pool.ntp.org iburst" >> /etc/ntp.conf
 
 cat /etc/ntp.conf > /opt/controlled_files/ntp.conf
 
 cat /opt/controlled_files/ntp.conf > /etc/ntp.conf
 
-/etc/init.d/ntp stop ; sleep 1 ; /etc/init.d/ntp start
+/etc/init.d/ntp restart
+# ; sleep 1 ; /etc/init.d/ntp start
+
 
 scr_dir=`pwd`
-(cat /etc/crontab | grep ntp_verify.sh) || echo "*/5* * * * root $scr_dir/ntp_verify.sh" >> /etc/crontab
+(cat /etc/crontab | grep ntp_verify.sh) || echo "* * * * * root $scr_dir/ntp_verify.sh" >> /etc/crontab
--- ntp_verify.sh
+++ ntp_verify.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+ntp_conf=/etc/ntp.conf
+ntp_conf_orig=/opt/controlled_files/ntp.conf
+
+ntp_stat=`ps ax | grep -v grep | grep ntpd | wc -l`
+
+if [ $ntp_stat -eq "0" ];
+then
+    echo "NOTICE: ntp is not running"
+    /etc/init.d/ntp start
+fi
+
+change_ntp=`diff -q $ntp_conf_orig $ntp_conf |wc -l`
+
+if [ $change_ntp -eq "1" ];
+then
+    echo "NOTICE: $ntp_conf was changed. Calculated diff:"
+    diff -U 0 $ntp_conf_orig $ntp_conf
+    cat $ntp_conf_orig > $ntp_conf
+    /etc/init.d/ntp restart
+fi
